92bff9f9b15bc2291ab42c56be8927e6adc9c110
Rafael Armelin <raffael.armelin@gmail.com>
2021-12-03 12:04:36 +0000

    sign up and login forms with auth done

---
+++ tattoo-app/src/components/SignUpForm.jsx
@@ -0,0 +1,88 @@
+import React from "react";
+import { useEffect, useState } from "react";
+
+export default function SignUp() {
+  const [authenticatedUser, setAuthenticatedUser] = useState(null);
+
+  const [userName, setUserName] = useState("");
+  const [userEmail, setUserEmail] = useState("");
+  const [userPassword, setUserPassword] = useState("");
+
+  const handleNameInput = (e) => {
+    setUserName(e.target.value);
+  };
+
+  const handleEmailInput = (e) => {
+    setUserEmail(e.target.value);
+  };
+
+  const handlePasswordInput = (e) => {
+    setUserPassword(e.target.value);
+  };
+
+  //   console.log("User", userName, userEmail, userPassword);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    const fetchOptions = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        name: userName,
+        email: userEmail,
+        password: userPassword,
+      }),
+    };
+
+    fetch("http://localhost:3030/auth/signup", fetchOptions)
+      .then((res) => res.json())
+      .catch(console.log)
+      .then((token) => {
+        console.log("token: ", token);
+        // const token = data.token;
+
+        if (token) {
+          setAuthenticatedUser(token);
+
+          localStorage.setItem("token", token);
+        }
+      });
+  };
+  return (
+    <form
+      className="form-stack light-shadow center user-form"
+      onSubmit={handleSubmit}
+    >
+      <h1>Sign Up</h1>
+      <label htmlFor="name-input">Name:</label>
+      <input
+        type="text"
+        name="name-input"
+        onChange={handleNameInput}
+        required
+      />
+      <label htmlFor="email-input">Email:</label>
+      <input
+        type="text"
+        name="email-input"
+        onChange={handleEmailInput}
+        required
+      />
+      <label htmlFor="password-input">Password:</label>
+      <input
+        type="password"
+        name="password-input"
+        onChange={handlePasswordInput}
+        required
+      />
+      <div className="actions-section">
+        <button className="button blue" type="submit">
+          Sign Up
+        </button>
+      </div>
+    </form>
+  );
+}
